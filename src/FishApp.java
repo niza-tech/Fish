6f37b325ae058cdc01a31a4f1e9d3fee0749d5f9
mobasel1234 <moya0002@stud.kea.dk>
2025-02-28 14:19:17 +0100

    app

--- src/FishApp.java
+++ src/FishApp.java
@@ -1,2 +1,45 @@
+import java.util.Scanner;
+
 public class FishApp {
+    Scanner scanner = new Scanner(System.in); //flyt til main senere?
+
+
+    //Menu (mangler)
+    public static void fishTankApp (Scanner scanner) {
+        int choice;
+
+        while (true) {
+            System.out.println("Welcome to the Fish tank App");
+            System.out.println("1. View fish status");
+            System.out.println("2. Update fish health");
+            System.out.println("3. Change water");
+            System.out.println("4. View last water change");
+            System.out.println("5. Exit menu");
+
+            choice = scanner.nextInt();
+
+            switch (choice) {
+                case 1:
+                    System.out.println("List of fish status...");
+                    break;
+                case 2:
+                    System.out.println("Updating fish health...");
+                    break;
+                case 3:
+                    System.out.println("Changing water...");
+                    break;
+                case 4:
+                    System.out.println("Viewing last water change...");
+                    break;
+                case 5:
+                    System.out.println("Exited menu");
+                    scanner.close();
+                    return;
+                default:
+                    System.out.println("Invalid choice try again!");
+
+            }
+            System.out.println("Returned to menu");
+        }
+    }
 }

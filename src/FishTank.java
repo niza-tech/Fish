bad0f0310c66e1bb3773d9bbfbcd010925776c75
mobasel1234 <moya0002@stud.kea.dk>
2025-02-28 14:20:43 +0100

    tank

--- src/FishTank.java
+++ src/FishTank.java
@@ -1,3 +1,18 @@
+import java.time.LocalDate;
+
+
 public class FishTank {
 
+
+    private String lastWaterChange;
+    private String waterQuality;
+
+    public FishTank(String lastWaterChange, String waterQuality) {
+        this.lastWaterChange = lastWaterChange;
+        this.waterQuality = waterQuality;
+    }
+
+    public enum waterQuality {
+        perfekt, godt, okay, dårlig
+    }
 }
